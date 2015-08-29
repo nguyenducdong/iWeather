15445f39ad76d3012139439b8ec5929ffd2d910d
bsv-DongNguyen <dongnguyenduc@Dongs-MacBook-Pro.local>
2015-08-30 00:43:31 +0700

    Commit first time

--- iWeather/ViewController.m
+++ iWeather/ViewController.m
@@ -10,18 +10,74 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *location;
+
+@property (weak, nonatomic) IBOutlet UIButton *temperature;
+@property (weak, nonatomic) IBOutlet UIImageView *weatherIcon;
+@property (weak, nonatomic) IBOutlet UILabel *quote;
+
+@property (weak, nonatomic) IBOutlet UILabel *labelTemperature;
+
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSArray *quotes;
+    NSArray *locations;
+    NSArray *photoFiles;
+    BOOL flag;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    quotes = @[@"Một con ngựa đau cả tàu bỏ cỏ",@"Có công mài sắt có ngày nên kim",@"Chớ thấy sóng cả mà ngã tay chèo",@"Không gì quý hơn độc lập tự do hạnh phúc",@"Đi một ngày học một sàn khôn"];
+    
+    locations = @[@"TP HCM", @"Hà Nội",@"Đà Nẵng",@"Nha Trang",@"Cần Thơ",@"Tokyo"];
+    photoFiles = @[@"sunny",@"thunder",@"rain",@"windy"];
+    flag = NO;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+
+-(IBAction)updateWeather:(id)sender{
+    int quoteIndex = arc4random_uniform(quotes.count);//return một số ngẫu nhiên nhỏ hơn quotes.count-1
+    NSLog(@"%d",quoteIndex);
+    self.quote.text = quotes[quoteIndex];
+    
+    int locationIndex = arc4random_uniform(locations.count);
+    self.location.text = locations[locationIndex];
+    
+    int photoFileIndex = arc4random_uniform(photoFiles.count);
+    self.weatherIcon.image = [UIImage imageNamed:photoFiles[photoFileIndex]];
+    
+    NSString *stringTemperature = [NSString stringWithFormat:@"%2.1f",[self getTemperature]];
+    NSLog(@"%@",stringTemperature);
+    [self.temperature setTitle:stringTemperature forState:UIControlStateNormal];
+}
+
+-(float)getTemperature{
+    
+    return 14.0 + arc4random_uniform(18) + (float)arc4random() / (float)INT32_MAX;
+}
+
+-(IBAction)tapButtonTemperature:(id)sender{
+    UIButton *btn = (UIButton *)sender;
+    if (!flag) {
+        NSString *str = [btn titleForState:UIControlStateNormal];
+        float tempC = [str floatValue];
+        float Fahrenheit = tempC *1.8 + 32;
+        [btn setTitle:[NSString stringWithFormat:@"%.1f",Fahrenheit] forState:UIControlStateNormal];
+        flag = YES;
+        self.labelTemperature.text = @"F";
+    }else{
+        NSString *str = [btn titleForState:UIControlStateNormal];
+        float tempF = [str floatValue];
+        float Celsius = (tempF - 32)/1.8;
+        [btn setTitle:[NSString stringWithFormat:@"%.1f",Celsius] forState:UIControlStateNormal];
+        self.labelTemperature.text = @"C";
+        flag = NO;
+    }
+    
+    
 }
 
 @end
